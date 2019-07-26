314be0c6681ca1ff1b237eb072d5bb4426c62119
骑着毛驴去旅行 <1379613157@qq.com>
2019-07-26 16:21:22 +0800

    加入了标题、图片的功能

--- gallery/models.py
+++ gallery/models.py
@@ -4,5 +4,13 @@ from django.db import models
 
 
 class Gallery(models.Model):
-    description = models.CharField(max_length=100)
+    description = models.CharField(default='在这里写作品的简介', max_length=100)
+    image = models.ImageField(upload_to='images/', default='default.png')
+    title = models.CharField(default='作品标题', max_length=50)
+    # music = models.ImageField(upload_to='musics/', default='default.mp3')
+    # video = models.ImageField(upload_to='videos/', default='default.mp4')
+
+    def __str__(self):
+        return self.title
+
 
