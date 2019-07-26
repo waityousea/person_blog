ff0a5e13d544c9029bcf611587a022b094b70bf6
骑着毛驴去旅行 <1379613157@qq.com>
2019-07-26 18:04:47 +0800

    博客初步完成

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(default='博文标题', max_length=50)
+    data = models.DateField()
+    image = models.ImageField(upload_to='images/', default='default.png')
+    text = models.TextField(default='正文')
+
+    def __str__(self):
+        return self.title
